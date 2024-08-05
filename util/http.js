602513d0e3dd17e40cfaafdf4cf1cc569a87ee39
Godfrey Kwan <>
2024-08-05 14:05:56 -0700

    add fetch to context

--- util/http.js
+++ util/http.js
@@ -1,10 +1,28 @@
 import axios from 'axios';
 
-const rootURL = 'https://react-nactive-test-default-rtdb.firebaseio.com'
+const ROOT_URL = 'https://react-nactive-test-default-rtdb.firebaseio.com'
 const expenseSegment = '/expenses.json'
 
 export function storeExpense(expenseData) {
   axios.post(
-    rootURL + expenseSegment,
+    ROOT_URL + expenseSegment,
     expenseData);
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(
+    ROOT_URL + expenseSegment
+  )
+
+  const expenses = [];
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description
+    }
+    expenses.push(expenseObj);
+  }
+  return expenses;
 }
